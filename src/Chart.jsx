1db0a94a22197ff3da85e3e8280dc45f8c388d06
votanak <v0tanak@yandex.ru>
2025-02-23 00:39:17 +0300

    asdads

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -12,6 +12,20 @@ for (let i = 1; i < 20000; i++) {
   data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
 }
 
+let base1 = +new Date(1986, 9, 3);
+let oneDay1 = 24 * 3600 * 1000;
+let date1 = [];
+let data1 = [Math.random() * 300];
+for (let i = 1; i < 20000; i++) {
+  var now1 = new Date((base1 += oneDay1));
+  date1.push(
+    [now1.getFullYear(), now1.getMonth() + 1, now1.getDate()].join('/'),
+  );
+  data1.push(Math.round((Math.random() - 0.5) * 20 + data1[i - 1]));
+}
+
+console.log(data);
+
 const ChartComponent = () => {
   const chartRef = useRef(null);
 
@@ -26,19 +40,15 @@ const ChartComponent = () => {
           return [pt[0], '10%'];
         },
       },
+      legend: {
+        show: 'true',
+        pageButtonPosition: 'start',
+        bottom: 'top',
+      },
       title: {
         left: 'center',
         text: 'Диаграмма значений',
       },
-      // toolbox: {
-      //   // feature: {
-      //   //   dataZoom: {
-      //   //     yAxisIndex: 'none',
-      //   //   },
-      //   //   restore: {},
-      //   //   saveAsImage: {},
-      //   // },
-      // },
       xAxis: {
         type: 'category',
         boundaryGap: false,
@@ -61,7 +71,7 @@ const ChartComponent = () => {
       ],
       series: [
         {
-          name: 'Random Data',
+          name: 'Random Data1',
           type: 'line',
           symbol: 'none',
           sampling: 'lttb',
@@ -81,6 +91,28 @@ const ChartComponent = () => {
           //   ]),
           // },
           data: data,
+        },
+        {
+          name: 'Random Data2',
+          type: 'line',
+          symbol: 'none',
+          sampling: 'lttb',
+          itemStyle: {
+            color: 'rgb(70, 70, 131)',
+          },
+          // areaStyle: {
+          //   color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          //     {
+          //       offset: 0,
+          //       color: 'rgb(255, 158, 68)',
+          //     },
+          //     {
+          //       offset: 1,
+          //       color: 'rgb(255, 70, 131)',
+          //     },
+          //   ]),
+          // },
+          data: data1,
         },
       ],
     };
